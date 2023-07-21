e58e84cb69a6799bcce176993fd8be27bda42333
judeclark19 <judeclark2019@gmail.com>
2023-07-21 17:57:15 -0400

    reading from firestore

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,128 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
 
-export default function Home() {
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+import { firestore } from "../../firebase/clientApp";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  collection,
+  doc,
+  getDocs,
+  limit,
+  query,
+  setDoc,
+  where,
+} from "firebase/firestore";
+
+const usersCollection = collection(firestore, "users");
+const gamesCollection = collection(firestore, "games");
+const scoresCollection = collection(firestore, "scores");
+
+const Home: NextPage = () => {
+  const [users, setUsers] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+  const [games, setGames] = useState<QueryDocumentSnapshot<DocumentData>[]>([]);
+  const [scores, setScores] = useState<QueryDocumentSnapshot<DocumentData>[]>(
+    []
+  );
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const getData = async () => {
+    const usersQuery = query(usersCollection);
+    const gamesQuery = query(gamesCollection);
+    const scoresQuery = query(scoresCollection);
+
+    const usersSnapshot = await getDocs(usersQuery);
+    const gamesSnapshot = await getDocs(gamesQuery);
+    const scoresSnapshot = await getDocs(scoresQuery);
+
+    const usersResult: QueryDocumentSnapshot<DocumentData>[] = [];
+    usersSnapshot.forEach((snapshot) => {
+      usersResult.push(snapshot);
+    });
+    setUsers(usersResult);
+
+    const gamesResult: QueryDocumentSnapshot<DocumentData>[] = [];
+    gamesSnapshot.forEach((snapshot) => {
+      gamesResult.push(snapshot);
+    });
+    console.log("gmaesresult", gamesResult[0].data());
+    setGames(gamesResult);
+
+    const scoresResult: QueryDocumentSnapshot<DocumentData>[] = [];
+    scoresSnapshot.forEach((snapshot) => {
+      scoresResult.push(snapshot);
+    });
+    console.log("scoresresult", scoresResult[0].data());
+    setScores(scoresResult);
+  };
+
+  useEffect(() => {
+    getData();
+    // reset loading
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.container}>
+      <Head>
+        <title>Todos app</title>
+        <meta name="description" content="Next.js firebase todos app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <h1 className={styles.title}>list of users</h1>
+
+        <div className={styles.grid}>
+          {loading ? (
+            <div className={styles.card}>
+              <h2>Loading</h2>
+            </div>
+          ) : users.length === 0 ? (
+            <div className={styles.card}>
+              <h2>No users</h2>
+            </div>
+          ) : (
+            users.map((user) => {
+              const { username, email } = user.data();
+              return (
+                // <p key={user.id}>{username}</p>
+                <div key={user.id} className={styles.card}>
+                  <h2>{username}</h2>
+                  <p>{email}</p>
+                  <h3>Scores:</h3>
+                  <ul>
+                    {scores.map((score) => {
+                      const { userId, gameId, score: userScore } = score.data();
+                      if (userId === user.id) {
+                        return (
+                          <li key={score.id}>
+                            <p>
+                              Game:{" "}
+                              {
+                                // find the game name by id
+                                games.find((game) => game.id === gameId)?.data()
+                                  .title
+                              }
+                            </p>
+                            <p>Score: {userScore}</p>
+                          </li>
+                        );
+                      }
+                    })}
+                  </ul>
+                </div>
+              );
+            })
+          )}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+      </main>
+    </div>
+  );
+};
+export default Home;
