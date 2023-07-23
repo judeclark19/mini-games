c848675a18b64121f5bc1c3208ce90932ddd0a92
judeclark19 <judeclark2019@gmail.com>
2023-07-22 21:48:13 -0400

    log in with email or google auth, login persists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "./page.module.css";
 import { useEffect, useState } from "react";
+import {
+  signInWithGoogle,
+  signUpWithEmail,
+  signOutFirebase,
+} from "@/firebase/clientApp";
 
 const Home: NextPage = () => {
   const [users, setUsers] = useState<any[]>([]);
@@ -39,52 +44,71 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>title</title>
-        <meta name="description" content="Next.js firebase todos app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>list of users</h1>
-        {loading ? (
-          <p>loading</p>
-        ) : (
-          <div className={styles.grid}>
-            {users.map((user) => {
-              const { username, email } = user;
-              return (
-                // <p key={user.id}>{username}</p>
-                <div key={user.id} className={styles.card}>
-                  <h2>{username}</h2>
-                  <p>{email}</p>
-                  <h3>Scores:</h3>
-                  <ul>
-                    {scores.map((score) => {
-                      const { userId, gameId, score: userScore } = score;
-                      if (userId === user.id) {
-                        return (
-                          <li key={score.id}>
-                            <p>
-                              Game:
-                              {
-                                // find the game name by id
-                                games.find((game) => game.id === gameId).title
-                              }
-                            </p>
-                            <p>Score: {userScore}</p>
-                          </li>
-                        );
-                      }
-                    })}
-                  </ul>
-                </div>
-              );
-            })}
-          </div>
-        )}
-      </main>
-    </div>
+    <>
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <button onClick={signInWithGoogle}>Sign in with google</button>
+          <>
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                const formData = new FormData(e.target as HTMLFormElement);
+                // console.log(formData.get("email"));
+                // console.log(formData.get("password"));
+                signUpWithEmail(
+                  formData.get("username") as string,
+                  formData.get("email") as string,
+                  formData.get("password") as string
+                );
+              }}
+            >
+              <input type="text" placeholder="username" name="username" />
+              <input type="email" placeholder="email" name="email" />
+              <input type="password" placeholder="password" name="password" />
+              <button type="submit">submit</button>
+            </form>
+            <button onClick={signOutFirebase}>Sign out</button>
+          </>
+          <h1 className={styles.title}>list of users</h1>
+          {loading ? (
+            <p>loading</p>
+          ) : (
+            <div className={styles.grid}>
+              {users.map((user) => {
+                const { username, email } = user;
+                return (
+                  // <p key={user.id}>{username}</p>
+                  <div key={user.id} className={styles.card}>
+                    <h2>{username}</h2>
+                    <p>{email}</p>
+                    <h3>Scores:</h3>
+                    <ul>
+                      {scores.map((score) => {
+                        const { userId, gameId, score: userScore } = score;
+                        if (userId === user.id) {
+                          return (
+                            <li key={score.id}>
+                              <p>
+                                Game:
+                                {
+                                  // find the game name by id
+                                  games.find((game) => game.id === gameId).title
+                                }
+                              </p>
+                              <p>Score: {userScore}</p>
+                            </li>
+                          );
+                        }
+                      })}
+                    </ul>
+                  </div>
+                );
+              })}
+            </div>
+          )}
+        </main>
+      </div>
+    </>
   );
 };
 export default Home;
--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -1,7 +1,19 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { doc, getFirestore, serverTimestamp, setDoc } from "firebase/firestore";
+import {
+  GoogleAuthProvider,
+  browserSessionPersistence,
+  createUserWithEmailAndPassword,
+  getAuth,
+  onAuthStateChanged,
+  setPersistence,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 
-initializeApp({
+const app = initializeApp({
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -11,4 +23,140 @@ initializeApp({
 });
 
 const firestore = getFirestore();
-export { firestore };
+const auth = getAuth(app);
+
+const provider = new GoogleAuthProvider();
+
+setPersistence(auth, browserSessionPersistence)
+  .then(() => {
+    // Existing and future Auth states are now persisted in the current session
+    // You can also use 'local' or 'none' if you prefer different persistence options
+    console.log("Session persistence enabled");
+  })
+  .catch((error) => {
+    console.log("Error setting session persistence:", error);
+  });
+
+const signInWithGoogle = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const name = result.user.displayName;
+      const email = result.user.email;
+      console.log(name, email);
+
+      localStorage.setItem("name", name as string);
+      localStorage.setItem("email", email as string);
+
+      // Create a user document in Firestore with the user's UID as the document ID
+
+      const userDocRef = doc(firestore, "users", result.user.uid);
+
+      // Set the user document fields
+      setDoc(userDocRef, {
+        createdAt: serverTimestamp(), // Firestore server timestamp
+        username: name, // Using the display name as the username in this case
+        email: email,
+      })
+        .then(() => {
+          console.log("User document created successfully");
+          // Additional code after creating user document if needed
+        })
+        .catch((error) => {
+          console.error("Error creating user document:", error);
+          // Handle error if necessary
+        });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+const signUpWithEmail = (username: string, email: string, password: string) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+
+      // Set the display name of the user using the incoming username
+      if (auth.currentUser) {
+        updateProfile(auth.currentUser, {
+          displayName: username,
+        })
+          .then(() => {
+            console.log("Display name set successfully:", username);
+            // Additional code after setting the display name if needed
+
+            const userDocRef = doc(firestore, "users", user.uid);
+            setDoc(userDocRef, {
+              createdAt: serverTimestamp(), // Firestore server timestamp
+              username: username,
+              email: email,
+            }).then(() => {
+              console.log("User document created successfully");
+              // Additional code after setting the display name and creating user document if needed
+            });
+          })
+          .catch((error) => {
+            console.error("Error setting display name:", error);
+            // Handle error if necessary
+          });
+
+        console.log("User created", user);
+        // Additional code after user signup if needed
+      }
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+      // Handle error if necessary
+    });
+};
+
+const signInWithEmail = (email: string, password: string) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+const signOutFirebase = () => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      console.log("signed out");
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log("error signing out", error);
+    });
+};
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+
+    const uid = user.uid;
+    console.log("the logged in user is", user, uid);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    console.log("no user logged in");
+  }
+});
+
+export {
+  firestore,
+  auth,
+  signInWithGoogle,
+  signUpWithEmail,
+  signInWithEmail,
+  signOutFirebase,
+};
